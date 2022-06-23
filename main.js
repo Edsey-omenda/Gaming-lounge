ab50463a0bf28769877550c7ddff2d0099b71b18
Edsey omenda <omendajames0317@gmail.com>
2022-06-23 16:23:16 +0300

    Declares and initializes variables from the html to grab and activate js functions

--- main.js
+++ main.js
@@ -2,13 +2,22 @@ document.addEventListener("DOMContentLoaded", () => {
     //  getGames();
   });
 const url = "https://www.freetogame.com/api/games"
+let image = document.getElementById("cover")
+let title = document.querySelector("#title")
+let playtime = document.getElementById("playtime")
+let runtime = document.getElementById("runtime")
+let description = document.getElementById("game-details") 
+let level = document.getElementById("level") 
+let spButton = document.getElementById("select-game") 
+let comment = document.getElementById("comment") 
+let pButton = document.getElementById("comment-button") 
 
 let getGames = () =>{
     fetch(url)
     .then(response => console.log(response.json()))
     .then(games =>{
       games.map(game =>{
-          console.log(game)
+         let ul = document.querySelector("#game-list")
       })
     })
  
