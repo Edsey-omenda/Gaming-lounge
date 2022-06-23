8a7980e25238f7109901ee0a6d591988631a8d92
Edsey omenda <omendajames0317@gmail.com>
2022-06-23 16:13:42 +0300

    adds a map array method for iterating through the objects

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-     getGames();
+    //  getGames();
   });
 const url = "https://www.freetogame.com/api/games"
 
 let getGames = () =>{
     fetch(url)
     .then(response => console.log(response.json()))
+    .then(games =>{
+      games.map(game =>{
+          console.log(game)
+      })
+    })
+ 
+    .catch(error =>console.log(error))
 }
+
+getGames();
 
  
